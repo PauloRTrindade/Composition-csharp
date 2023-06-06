ed5fa7470252a1ed36f8615a53d70de2fc9cc16d
PauloRTrindade <prtrindadesf@gmail.com>
2023-06-05 21:51:01 -0300

    Created class 'Product'

---
+++ Composition/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Composition.Entities
+{
+    internal class Product
+    {
+
+        public string Name { get; set; }
+        public Double Price { get; set; }
+
+        public Product() { }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- Composition/Program.cs
+++ Composition/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using Composition.Entities;
 
 namespace Composition
 {
@@ -6,7 +8,14 @@ namespace Composition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Produto: ");
+            string Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            Double Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product product = new Product(Name, Price);
+
+            Console.WriteLine(product.Name + ", R$ " + product.Price.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
